990e88ef11a6779c556975069a9186a918755757
Hemanth Reddy <kalakotahemanthreddy24@gmail.com>
2024-07-09 10:20:37 +0530

    rearranged the code and turned into a flask app

--- app.py
+++ app.py
@@ -1,48 +1,35 @@
-from flask import Flask, render_template, request, jsonify
-import pandas as pd
-from sklearn.linear_model import LinearRegression
+# app.py
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Load the dataset
-data = pd.read_csv('diet_dataset.csv')
-
-# Data Preprocessing
-# (e.g., handle missing values, encode categorical variables)
-
-# Feature Engineering
-# (e.g., calculate BMI)
-
-# Train the model
-X = data[['age', 'weight(kg)', 'height(m)', 'gender']]  # Features
-y = data['calories_to_maintain_weight']  # Target variable
-model = LinearRegression()
-model.fit(X, y)
-
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/bmi')
+def bmi():
+    return render_template('bmi.html')
+
+@app.route('/calorie')
+def calorie():
     return render_template('calorie.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Get user input from the request
-    input_data = request.get_json()
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
-    # Extract input values
-    age = input_data['age']
-    weight = input_data['weight']
-    height = input_data['height']
-    gender = input_data['gender']
-
-    # Encode gender
-    gender_encoded = 1 if gender.lower() == 'male' else 0
-
-    # Make prediction
-    new_data = pd.DataFrame({'age': [age], 'weight(kg)': [weight], 'height(m)': [height], 'gender': [gender_encoded]})
-    predicted_calories = model.predict(new_data)
-
-    # Return the predicted result
-    return jsonify({'predicted_calories': predicted_calories[0]})
+@app.route('/diet')
+def diet():
+    return render_template('diet.html')
 
 
 if __name__ == '__main__':
